25ae1565dad4290cbef43d05647e586182cceea5
Potter <75838259@qq.com>
2024-07-10 09:56:42 +0800

    fix(docs): load static resource path mistake

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vitepress'
+import { createRequire } from 'module'
 import { en } from './en'
 import { zh } from './zh'
+
+const require = createRequire(import.meta.url)
+const pkg = require('../../../package.json')
+const name: string = pkg.name.split('/').pop();
 
 export default defineConfig({
   locales: {
     root: { label: 'English', ...en },
     zh: { label: '简体中文', ...zh },
   },
+  base: `/${name}/`,
   themeConfig: {
-    siteTitle: 'artipub'
+    siteTitle: name
   }
 })
