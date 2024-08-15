e423581bbc80bc9f3d9f6736ef2644165a321695
Potter <aa4790139@gmail.com>
2024-08-15 22:43:25 +0800

    fix: ajv output schema warning

--- packages/cli/src/constant.ts
+++ packages/cli/src/constant.ts
@@ -20,41 +20,41 @@ Options:
   `;
 }
 
-export const nativePlatformSchema: JSONSchemaType<{ name: "blog"; destination_path: string; cdn_prefix?: string; res_domain?: string }> = {
+type NativePlatformData = { destination_path: string; cdn_prefix?: string; res_domain?: string };
+export const nativePlatformSchema: JSONSchemaType<NativePlatformData> = {
   type: "object",
   properties: {
-    name: { type: "string", const: "blog" },
     destination_path: { type: "string" },
     cdn_prefix: { type: "string", nullable: true },
     res_domain: { type: "string", nullable: true },
   },
-  required: ["name", "destination_path"],
+  required: ["destination_path"],
   additionalProperties: false,
 };
 
-export const notionPlatformSchema: JSONSchemaType<{ name: "notion"; api_key: string; data_base_id: string; page_id: string }> = {
+type NotionPlatformData = { api_key: string; data_base_id?: string; page_id: string };
+export const notionPlatformSchema: JSONSchemaType<NotionPlatformData> = {
   type: "object",
   properties: {
-    name: { type: "string", const: "notion" },
     api_key: { type: "string" },
-    data_base_id: { type: "string" },
+    data_base_id: { type: "string", nullable: true },
     page_id: { type: "string" },
   },
-  required: ["name", "api_key", "data_base_id", "page_id"],
+  required: ["api_key", "page_id"],
 };
 
-export const devToPlatformSchema: JSONSchemaType<{
-  name: "dev.to";
+type DevToPlatformData = {
   api_key: string;
   published?: boolean;
   series?: string;
   main_image?: string;
   description?: string;
   organization_id?: number;
-}> = {
+};
+
+export const devToPlatformSchema: JSONSchemaType<DevToPlatformData> = {
   type: "object",
   properties: {
-    name: { type: "string", const: "dev.to" },
     api_key: { type: "string" },
     published: { type: "boolean", nullable: true },
     series: { type: "string", nullable: true },
@@ -62,47 +62,64 @@ export const devToPlatformSchema: JSONSchemaType<{
     description: { type: "string", nullable: true },
     organization_id: { type: "number", nullable: true },
   },
-  required: ["name", "api_key"],
+  required: ["api_key"],
+};
+
+type GithubOptionData = {
+  owner: string;
+  repo: string;
+  dir: string;
+  branch: string;
+  token: string;
+  cdn_prefix: string;
+  commit_author: string;
+  commit_email: string;
+};
+
+const githubOptionSchema: JSONSchemaType<GithubOptionData> = {
+  type: "object",
+  properties: {
+    owner: { type: "string" },
+    repo: { type: "string" },
+    dir: { type: "string" },
+    branch: { type: "string" },
+    token: { type: "string" },
+    cdn_prefix: { type: "string" },
+    commit_author: { type: "string" },
+    commit_email: { type: "string" },
+  },
+  required: ["owner", "repo", "dir", "branch", "token", "cdn_prefix", "commit_author", "commit_email"],
+  additionalProperties: false,
 };
 
 export const schema: JSONSchemaType<{
-  githubOption: {
-    owner: string;
-    repo: string;
-    dir: string;
-    branch: string;
-    token: string;
-    cdn_prefix: string;
-    commit_author: string;
-    commit_email: string;
+  githubOption: GithubOptionData;
+  platforms: {
+    native?: NativePlatformData;
+    notion?: NotionPlatformData;
+    "dev.to"?: DevToPlatformData;
   };
-  platforms: object;
 }> = {
   type: "object",
   properties: {
-    githubOption: {
-      type: "object",
-      properties: {
-        owner: { type: "string" },
-        repo: { type: "string" },
-        dir: { type: "string" },
-        branch: { type: "string" },
-        token: { type: "string" },
-        cdn_prefix: { type: "string" },
-        commit_author: { type: "string" },
-        commit_email: { type: "string" },
-      },
-      required: ["owner", "repo", "dir", "branch", "token", "cdn_prefix", "commit_author", "commit_email"],
-      additionalProperties: false,
-    },
+    githubOption: githubOptionSchema,
     platforms: {
       type: "object",
-      items: {
-        //TODO: 类型定义还需要调整
-        anyOf: [nativePlatformSchema, notionPlatformSchema, devToPlatformSchema],
+      properties: {
+        native: {
+          ...nativePlatformSchema,
+          nullable: true,
+        },
+        notion: {
+          ...notionPlatformSchema,
+          nullable: true,
+        },
+        "dev.to": {
+          ...devToPlatformSchema,
+          nullable: true,
+        },
       },
     },
   },
-  required: ["githubOption", "platforms"],
-  additionalProperties: false,
+  required: ["platforms"],
 };
