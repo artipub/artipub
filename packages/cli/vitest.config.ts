3b60a8d86cd80ebabfa84d79f24559e304eb56c6
Potter <aa4790139@qq.com>
2024-08-16 14:59:12 +0800

    test(cli): add an independent vitest.config.js

---
+++ packages/cli/vitest.config.ts
@@ -0,0 +1,15 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vitest/config";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  test: {
+    testTimeout: 50 * 1000,
+    include: ["test/**/*.test.ts"],
+    alias: {
+      "@": resolve(__dirname, "src"),
+    },
+  },
+});
